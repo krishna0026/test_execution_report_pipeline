a170dc08f1bc59641a204ac989a05bce1a336b57
krishna0026 <krishnasinghadhikari26@gmail.com>
2025-11-18 08:22:10 +0000

    headless added

--- fb_login.py
+++ fb_login.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 def get_facebook_title():
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")               # RUN HEADLESS is necessary for using selenium using git
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+
     driver = webdriver.Chrome()
     driver.get('https://www.facebook.com/')
     title= driver.title
