d47510386da5569a1c05a7764014d078410d78af
krishna0026 <krishnasinghadhikari26@gmail.com>
2025-11-18 08:37:12 +0000

    code updated

--- fb_login.py
+++ fb_login.py
@@ -1,23 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 def get_facebook_title():
     chrome_options = Options()
-    chrome_options.add_argument("--headless=new")  # Required for GitHub Actions
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    chrome_options.add_argument("--remote-debugging-port=9222")
 
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://www.facebook.com")
+    chrome_options.binary_location = "/usr/bin/chromium-browser"
+
+    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    driver.get("https://facebook.com")
     title = driver.title
     driver.quit()
     return title
-
-
 
 # print('Title: ', get_facebook_title() )
 # This is to just check cod eis working fine or not
